59bfaa5edfe1f9ebb308261155981634bedc9c9a
Tran-Quyen <quyen10a8.sao@gmail.com>
2022-07-15 20:24:44 +0700

    📦 NEW: Finish desktop website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
+var authorLink = $('.footer-author > a')[0];
+
+var i = 0;
+setInterval(() => {
+  $(authorLink).css({
+    color: 'red',
+    filter: `hue-rotate(${i}deg)`,
+  });
+  if (i === 360) i = 0;
+  i += 20;
+}, 1000);
 
